708b71d2f781b493990c357c28d54cc08306f27a
Mikey <pride81_@hotmail.com>
2024-06-29 23:04:44 +0900

    Added image animation

--- app/(components)/PortraitImage.tsx
+++ app/(components)/PortraitImage.tsx
@@ -6,7 +6,7 @@ function PortraitImage() {
     <div className="bg-white w-full flex flex-col justify-center items-center py-5 font-mono">
       <h1 className="font-semibold text-3xl mb-10 text-center">J.H. KIM</h1>
       <Image
-        className="rounded-full shadow-2xl shadow-black"
+        className="rounded-full shadow-2xl shadow-black hover:size-[30%] duration-300"
         src="/images/photo_2024-06-29_20-17-55.jpg"
         alt="junghyunkim"
         width={200}
