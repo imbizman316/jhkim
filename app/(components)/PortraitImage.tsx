3cddc707e85863d8eae8f2f32ae7b2a64a1d3b95
Mikey <pride81_@hotmail.com>
2024-06-29 21:39:59 +0900

    first commit

---
+++ app/(components)/PortraitImage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+
+function PortraitImage() {
+  return (
+    <div className="min-h-screen bg-black flex justify-start items-center flex-col">
+      <div className="bg-white w-full flex flex-col justify-center items-center py-10 font-mono">
+        <h1 className="font-semibold text-3xl mb-10 text-center">J.H. KIM</h1>
+        <Image
+          className="rounded-full shadow-2xl shadow-black"
+          src="/images/photo_2024-06-29_20-17-55.jpg"
+          alt="junghyunkim"
+          width={200}
+          height={200}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PortraitImage;
